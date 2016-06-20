82e488a855613a09ff7569890bf4526ea874ca54
ls123456789 <2953757467@qq.com>
2016-06-20 17:24:17 +0800

    从1加到100
    先定义一个变量，用来存放相加得来的和。变量生命的关键字为var，给变量初始化后，可以自动检测变量的类型。使用一个for循环来运算1-100的相加
    。定义变量i，遍历i从1-100，在for循环中给add变量赋计算后的值，最后输出add变量的值。

---
+++ lession/lession/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  lession
+//
+//  Created by 梁爽 on 16/6/20.
+//  Copyright © 2016年 梁爽. All rights reserved.
+//
+
+import Foundation
+
+var add=0;
+for i in 1..<101{
+    add = add + i
+}
+print(add)
